c1d96a59ad79ecc8b10e5ad32f8e36d1361aeb4b
heewoung-lee <154584396+heewoung-lee@users.noreply.github.com>
2025-10-10 12:39:14 +0000

    Deploying to main from @ heewoung-lee/ImpossibleBosses@807fe3ec7e4fbca2b0ff17763def4352e760e90b 🚀

--- GameManagers/GameDataSpreadSheet.cs
+++ GameManagers/GameDataSpreadSheet.cs
@@ -1,16 +1,12 @@
-using GameManagers.Interface.DataManager;
+using UnityEngine;
 
-namespace GameManagers
+public class GameDataSpreadSheetDummy : MonoBehaviour
 {
-    public class GameDataSpreadSheet : IGameDataSpreadSheet,ILoginDataSpreadSheet
-    {
-        private const string _gameDataSpreadsheetID = "1DV5kuhzjcNs3id8deI8Q3xFgbOWTa_pr76uSD0gNpGg";
-        private const string _loginDataSpreadsheetID = "1SKhi41z1KRfHI6KwhQ2iM3mSjgLZKXw7_VopoIEZYNQ";
-        private const string _userAuthenticateDatasheetName  = "UserAuthenticateData";
-        
-        public string GameDataSpreadsheetID => _gameDataSpreadsheetID;
-        public string LoginDataSpreadsheetID => _loginDataSpreadsheetID;
-        public string UserAuthenticateDatasheetName => _userAuthenticateDatasheetName;
+    private const string _gameDataSpreadsheetID = "PublicValue";
+    private const string _loginDataSpreadsheetID = "PublicValue";
+    private const string _userAuthenticateDatasheetName = "PublicValue";
 
-    }
+    public string GameDataSpreadsheetID => _gameDataSpreadsheetID;
+    public string LoginDataSpreadsheetID => _loginDataSpreadsheetID;
+    public string UserAuthenticateDatasheetName => _userAuthenticateDatasheetName;
 }
